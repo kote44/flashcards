7be927f432501a35f9f1f3eb14099b13a67323db
kote44 <kotekote0404@gmail.com>
2020-07-09 10:52:22 +0300

    Initial commit after Gatsby setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+// Libs
+import React from "react"
+
+// Components
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+    <h1>This is a starting page for Flashcards app.</h1>
+  </Layout>
+)
+
+export default IndexPage

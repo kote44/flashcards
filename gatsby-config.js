2683919334cd7253daaaaf8ede0d8b93e6930637
kote44 <kotekote0404@gmail.com>
2020-07-13 18:56:08 +0300

    Established initial connection to mongo collection

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,27 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-mongodb`,
+      options: {
+        dbName: "flashcards-db",
+        collection: "cards",
+        server: {
+          address: "cluster0-shard-00-00.vbtyl.mongodb.net",
+          port: 27017,
+        },
+        auth: {
+          user: "dimas779",
+          password: "Rbi6LTQbmdEnkZgx",
+        },
+        extraParams: {
+          replicaSet: "Main-shard-0",
+          ssl: true,
+          authSource: "admin",
+          retryWrites: true,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
